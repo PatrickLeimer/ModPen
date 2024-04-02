a782903dbb25ad942eb13a10d77184b5eb510c9b
teoslivka@gmail.com <teoslivka@gmail.com>
2024-04-02 14:30:45 -0400

    adds tailwind styling

--- src/App.js
+++ src/App.js
@@ -8,29 +8,23 @@ import { ChakraProvider, Flex, Text } from '@chakra-ui/react'
 function App() {
   return (
     <ChakraProvider>
-    <div className="App">
+    <div class="bg-customBackground text-center">
       <Header/>
       <body>
         <Text fontSize='30px'>
           Welcome to the future of creation. Accessible for anybody, anywhere, to do anything.
         </Text>
-        <Flex justifyContent="space-between">
-          <ModelView/>
-          <DescriptionPanel/>
-        </Flex>
+        <div class="bg-customBackground">
+          <Flex justifyContent="space-between">
+            <ModelView/>
+            <DescriptionPanel/>
+          </Flex>
+        </div>
       </body>
       <footer>
-        <p>
-          Interested? Fill out the interest form below.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Interest Form
-        </a>
+        <div class="bg-customBackground p-4 h-90 w-50 rounded-lg" style={{position: 'center', top: '20px'}}>
+          <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfGkLBzpBpEXhogLSZSVtwiYhSA58p6lqki8ut263iDQ2ShjQ/viewform?embedded=true" width="640" height="382" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+        </div>
       </footer>
     </div>
     </ChakraProvider>
--- src/Components/DescriptionPanel.js
+++ src/Components/DescriptionPanel.js
@@ -5,9 +5,10 @@ import { Container, Box, Flex } from '@chakra-ui/react'
 function DescriptionPanel() {
     return (
         <div className='descriptionpanel'>
-            <Box bg='tomato' display="flex" justifyContent="space-between" w='600px' h='400px' >
+            <div class="bg-gray-200 p-4 h-60 w-100 rounded-lg" style={{position: 'relative', right: '115px', top: '20px'}}>
                 <h1>This will hold the view of the description of the model</h1>
-            </Box>
+                <div className="absolute inset-0 border border-gray-300 -m-1 rounded-lg pointer-events-none"></div>
+            </div>
         </div>
     )
 
